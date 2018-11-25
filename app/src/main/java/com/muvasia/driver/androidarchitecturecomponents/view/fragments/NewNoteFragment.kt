8f9b33d3e3c967a7f4fb8935741a1c26f61bb966
Shantu Das <shantudasbd@yahoo.com>
2018-11-25 18:49:55 +0600

    insert new note feature implemented

--- app/src/main/java/com/muvasia/driver/androidarchitecturecomponents/view/fragments/NewNoteFragment.kt
+++ app/src/main/java/com/muvasia/driver/androidarchitecturecomponents/view/fragments/NewNoteFragment.kt
@@ -1,13 +1,18 @@
 package com.muvasia.driver.androidarchitecturecomponents.view.fragments
 
 
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import com.muvasia.driver.androidarchitecturecomponents.database.Note
 
 import com.muvasia.driver.androidarchitecturecomponents.R
+import com.muvasia.driver.androidarchitecturecomponents.viewModel.NoteViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,6 +24,8 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class NewNoteFragment : Fragment() {
+
+    private lateinit var noteViewModel: NoteViewModel
 
     companion object {
         val TAG = NewNoteFragment::class.simpleName
@@ -33,8 +40,34 @@ class NewNoteFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_new_note, container, false)
+
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel::class.java)
+
+
+        var etTitle = view.findViewById(R.id.etTitle) as EditText
+        var etBody = view.findViewById(R.id.etBody) as EditText
+        var btnSubmit = view.findViewById(R.id.btnSubmit) as Button
+
+        btnSubmit.setOnClickListener {
+            var title = etTitle.text.toString().trim()
+            var body = etBody.text.toString().trim()
+
+            if (!title.isEmpty() || !body.isEmpty()) {
+                var note = Note(title,body)
+                noteViewModel.insert(note)
+
+                activity!!.supportFragmentManager
+                    .beginTransaction()
+                    .addToBackStack(NoteListFragment.TAG)
+                    .replace(R.id.fragment_container, NoteListFragment.newInstance(), NoteListFragment.TAG)
+                    .commit()
+            }
+        }
+
         return view
     }
 
